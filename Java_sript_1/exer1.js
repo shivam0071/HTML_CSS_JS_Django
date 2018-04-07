8cedef5569358b05378155886dabb7e629232fe4
Shivam <shivam.koundal00@gmail.com>
2018-04-07 16:20:59 +0530

    Java Script

---
+++ Java_sript_1/exer1.js
@@ -0,0 +1,2 @@
+var weight = prompt("Enter the weight in lbs")
+alert("The Weight in KGs is "+ parseInt(weight) * 0.454+ " kg") 
---
+++ Java_sript_1/script1.js
@@ -0,0 +1,3 @@
+alert('Welome, How many bags do you hodl?')
+var bags = prompt('Enter the number of bags')
+alert('Wow! '+bags+' Thanks for buying my bags')
