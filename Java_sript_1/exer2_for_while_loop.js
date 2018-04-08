02096357a6a1509e45f09ec3ab83f809a7c2eb09
Shivam <shivam.koundal00@gmail.com>
2018-04-08 16:52:20 +0530

    for each

--- Java_sript_1/exer2_for_while_loop.js
+++ Java_sript_1/exer2_for_while_loop.js
@@ -9,3 +9,15 @@ while (num < 26) {
   console.log(num);
   num += 1;
 }
+
+// a = 'ABC'
+// for (l of a){console.log(l)}
+// A
+// B
+// C
+
+
+// b = ['A','BQ','CD'];
+// b.forEach(alert)
+// notice the fumction dont have parenthesis here..so that means we are passing the
+// array values here
