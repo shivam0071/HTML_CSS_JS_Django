b2dec865b6ff1ef0a267958bc17a257364d397aa
shivam0071 <shivam.koundal00@gmail.com>
2018-04-14 12:02:24 +0530

    Eveything

---
+++ JavaScript2/fun_1.js
@@ -0,0 +1,16 @@
+function normal() {
+  console.log('welcome to the world of functions');
+}
+
+function enterName(name="Anon") {
+console.log('Hello '+name);
+}
+
+function addNum(a,b) {
+  return a+b;
+}
+
+
+normal()
+enterName('shaan')
+addNum(33,44)
