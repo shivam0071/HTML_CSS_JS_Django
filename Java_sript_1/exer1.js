d4f26abc7225db2760b1ccf9687220ea1768d383
shivam0071 <shivam.koundal00@gmail.com>
2018-04-15 12:40:19 +0530

    duplicates

--- Java_sript_1/exer1.js
+++
@@ -1,4 +0,0 @@
-var weight = prompt("Enter the weight in lbs");
-var kg = weight*0.454;
-alert("The Weight in KGs is "+ parseInt(weight) * 0.454+ " kg");
-console.log("Success "+kg); 
--- Java_sript_1/script1.js
+++
@@ -1,3 +0,0 @@
-alert('Welome, How many bags do you hodl?')
-var bags = prompt('Enter the number of bags')
-alert('Wow! '+bags+' Thanks for buying my bags')
