6f2aef6fdaac5543db1e4550bbbcb9f30cac8cb6
shivam0071 <shivam.koundal00@gmail.com>
2018-04-15 12:20:11 +0530

    color

--- jQuery/game.js
+++ jQuery/game.js
@@ -1,14 +1,14 @@
-player_name = [prompt('Player One Enter your name, you will be blue!')]
-player_name.push('blue')
-// Player One: It is your turn,please pick up a cloumn to drop your blue chip.
-player_name2 = [prompt('Player Two Enter your name, you will be red!')]
-player_name2.push('red')
-
-// flag = true;
-// function whosTurn() {
-//   if ()
-//   {
-    promt_text = player_name[0]+' it is your turn,please pick up a column to drop your '+player_name[1]+' chips'
+// player_name = [prompt('Player One Enter your name, you will be blue!')]
+// player_name.push('blue')
+// // Player One: It is your turn,please pick up a cloumn to drop your blue chip.
+// player_name2 = [prompt('Player Two Enter your name, you will be red!')]
+// player_name2.push('red')
+//
+// // flag = true;
+// // function whosTurn() {
+// //   if ()
+// //   {
+//     promt_text = player_name[0]+' it is your turn,please pick up a column to drop your '+player_name[1]+' chips'
 //   }
 //   else {
 //     promt_text = player_name[2]+' it is your turn,please pick up a column to drop your '+player_name[2]+' chips'
@@ -16,16 +16,22 @@ player_name2.push('red')
 //   flag = false;
 // }
 // whosTurn()
-function demo() {
-  console.log('demo');
+// $('h4').text(promt_text);
+
+function randomColor() {
+var letters =  "0123456789ABCDEF";
+var color = '#';
+for (var i = 0; i < 6; i++) {
+  color += letters[Math.floor(Math.random()*16)];
+}
+return color;
 }
 
-$('h4').text(promt_text);
 function row_click() {
   console.log('Clicked');
   console.log(this);
   butt = this.querySelector('button')
-  butt.style.background = 'blue';
+  butt.style.background = randomColor();
   // butt.setAttribute('class','blue ')
   // console.log(butt.);
  // for (var i = 0; i < this.length; i++) {
